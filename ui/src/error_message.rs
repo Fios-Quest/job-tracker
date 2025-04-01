285ba2c8d6b40c21a7383e780b8ea76c7f6a259d
Daniel Mason <daniel@danielmason.com>
2025-04-01 22:59:38 +0100

    feat: handle errors in ui without crashing the whole app :)

---
+++ ui/src/error_message.rs
@@ -0,0 +1,11 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn ErrorMessage(message: String) -> Element {
+    rsx! {
+        div {
+            class: "error-message",
+            {message},
+        }
+    }
+}
