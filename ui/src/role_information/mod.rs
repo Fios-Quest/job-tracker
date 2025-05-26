d5464ce06951d84ee48b36f59f959128c3fbeafb
Daniel Mason <daniel@danielmason.com>
2025-05-26 21:14:37 +0100

    feat: role description now updates immediately
    Also simplified the logic for that element (a bit)

--- ui/src/role_information/mod.rs
+++ ui/src/role_information/mod.rs
@@ -20,44 +20,46 @@ pub fn RoleDescription(role: Option<Role>) -> Element {
 
     let mut form_receiver: Signal<Option<Event<FormData>>> = use_signal(|| None);
 
-    match role_resource().unwrap_or_default() {
-        Some(role) => {
-            let input_name = "role_description";
+    let Some(role) = role_resource().unwrap_or_default() else {
+        return rsx! {};
+    };
 
-            if let Some(event) = form_receiver() {
-                let stores = stores.clone();
-                let role = role.clone();
-                let role_description = event.values().get(input_name).map(|v| v.as_value());
-                spawn(async move {
-                    if let Some(description) = role_description {
-                        if !description.is_empty() {
-                            let role = Role {
-                                description: description.clone(),
-                                ..role
-                            };
-                            let _result = stores.update_role(&role).await;
-                            let new_context = application_context().set_role(role.clone()).unwrap(); // ToDo: Fix me
-                            application_context.set(new_context);
-                            role_resource.restart();
-                            form_receiver.set(None);
-                        }
-                    }
-                });
-            }
+    let input_name = "role_description";
 
-            let editable = rsx! {
-                textarea {
-                    name: input_name,
-                    "{role.description}"
+    if let Some(event) = form_receiver() {
+        let stores = stores.clone();
+        let role = role.clone();
+        let role_description = event.values().get(input_name).map(|v| v.as_value());
+        spawn(async move {
+            if let Some(description) = role_description {
+                if !description.is_empty() {
+                    let role = Role {
+                        description: description.clone(),
+                        ..role
+                    };
+                    let _result = stores.update_role(&role).await;
+                    let new_context = application_context().set_role(role.clone()).unwrap(); // ToDo: Fix me
+                    application_context.set(new_context);
+                    role_resource.restart();
+                    form_receiver.set(None);
                 }
-            };
-            let display = rsx! { PopulatedRoleDescription { role } };
-
-            rsx! {
-                h3 { "Role Description" }
-                Editable { display, editable, form_receiver }
             }
+        });
+    }
+
+    let editable = rsx! {
+        textarea { name: input_name, "{role.description}" }
+    };
+    let display = rsx! {
+        PopulatedRoleDescription { role }
+    };
+
+    rsx! {
+        h3 { "Role Description" }
+        if form_receiver().is_none() {
+            Editable { display, editable, form_receiver }
+        } else {
+            "pending"
         }
-        None => rsx!(),
     }
 }
