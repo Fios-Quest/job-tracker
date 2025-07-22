9817a25f6873e4ea7d1a57905a97c17785d7eea7
Daniel Mason <daniel@danielmason.com>
2025-07-22 12:53:12 +0100

    feat(storage): allow more generic impl of Deserialize for Partials

--- storage/src/storable/property/apply_partial.rs
+++ storage/src/storable/property/apply_partial.rs
@@ -1,17 +1,18 @@
 use partially::Partial;
 use serde::de::DeserializeOwned;
+use serde::Deserialize;
 
-pub trait ApplyPartial<P>
+pub trait ApplyPartial<'a, P>
 where
     Self: Partial<Item = P>,
-    P: DeserializeOwned,
+    P: Deserialize<'a>,
 {
     fn apply(&mut self, partial: P) -> bool {
         Partial::apply_some(self, partial)
     }
 }
 
-impl<T, P> ApplyPartial<P> for T
+impl<'a, T, P> ApplyPartial<'a, P> for T
 where
     T: Partial<Item = P>,
     P: DeserializeOwned,
