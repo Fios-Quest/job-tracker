7eb17a69730bc31c2f9afe0394d10e404b7b2437
Daniel Mason <daniel@danielmason.com>
2025-08-04 09:28:12 +0100

    feat(ui): use new partial features to edit roles

--- ui/src/views/home/details/role_information/mod.rs
+++ ui/src/views/home/details/role_information/mod.rs
@@ -1,10 +1,11 @@
+use application_context::prelude::ApplicationContext;
 use dioxus::prelude::*;
 use std::sync::Arc;
 
 mod populated_role_description;
+use crate::helpers::ModifyWithFormData;
 use crate::router::DetailsView;
 use crate::{Editable, Route, StoreType};
-use application_context::prelude::*;
 use populated_role_description::PopulatedRoleDescription;
 use storage::prelude::*;
 
@@ -12,46 +13,40 @@ use storage::prelude::*;
 pub fn RoleDescription(role: Arc<Role>) -> Element {
     let stores = use_context::<StoreType>();
     let mut context = use_context::<Signal<ApplicationContext>>();
-
-    let input_name = "role_description";
-
-    let mut form_receiver: Signal<Option<Event<FormData>>> = use_signal(|| None);
-    if let Some(event) = form_receiver() {
-        let mut stores = stores.clone();
-        let role_description = event.values().get(input_name).map(|v| v.as_value());
-        let role = role.clone();
-        spawn(async move {
-            if let Some(description) = role_description {
-                if !description.is_empty() {
-                    let edited_role = Role {
-                        description,
-                        ..role.as_ref().clone()
-                    };
-                    let _result = stores.store(edited_role.clone()).await;
-                    let nav = navigator();
-                    nav.push(Route::HomeRole {
-                        company_id: edited_role.company_id,
-                        role_id: edited_role.id,
-                        view: DetailsView::Role,
-                    });
-                    form_receiver.set(None);
-                    // ToDo: Why isn't this updating the role in context?! 😡
-                    context.set(
-                        context()
-                            .set_role(edited_role)
-                            .expect("Couldn't set the role"),
-                    );
-                }
-            }
-        });
-    }
+    let input_name = "description";
 
     let editable = rsx! {
         textarea { name: input_name, "{role.description}" }
     };
     let display = rsx! {
-        PopulatedRoleDescription { role }
+        PopulatedRoleDescription { role: role.clone() }
     };
+
+    let mut form_receiver: Signal<Option<Event<FormData>>> = use_signal(|| None);
+    if let Some(event) = form_receiver() {
+        let mut stores = stores.clone();
+        let mut role = Arc::unwrap_or_clone(role);
+        role.modify_with_form_data(&event)
+            .expect("Could not modify role with form data");
+        let role_id = role.id;
+        let company_id = role.company_id;
+        spawn(async move {
+            stores
+                .store(role.clone())
+                .await
+                .expect("Could not store role");
+            let new_context = context()
+                .set_role(role)
+                .expect("Could not set role in app context");
+            context.set(new_context);
+            navigator().push(Route::HomeRole {
+                company_id,
+                role_id,
+                view: DetailsView::Role,
+            });
+            form_receiver.set(None);
+        });
+    }
 
     rsx! {
         h3 { "Role Description" }
